21ea67b9159959cb9e502b03101f4fe7a3e0eae7
Security-Development <35417717+Security-Development@users.noreply.github.com>
2022-03-25 13:02:38 +0900

    Delete example.ts

--- asynQuery./example.ts
+++
@@ -1,8 +0,0 @@
-import { Query } from "./be";
-const query: Query = new Query("jkonline.mcok.kr", 19132);
-
-query.getData()
-    .then(data => {
-        console.log(data);
-        query.close();
-    });
