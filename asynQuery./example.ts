1ffbe9fcb51a462513490a3c2ec07fada06477e5
Security-Development <35417717+Security-Development@users.noreply.github.com>
2022-03-25 19:39:14 +0900

    Update example.ts

--- asynQuery./example.ts
+++ asynQuery./example.ts
@@ -1,5 +1,5 @@
 import { Query } from "./be";
-const query: Query = new Query("123.123.123.123", 19132);
+const query: Query = new Query("123.123.123.123", "19132");
 
 query.getData()
     .then(data => {
